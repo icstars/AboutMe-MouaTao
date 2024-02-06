da00492d0f8ccdaefa6e2e5f54c76df27ac9cbc2
mouataocha <149593805+mouataocha@users.noreply.github.com>
2024-02-06 10:24:44 -0600

    Update 2/6

--- Phaser Assignment/index.js
+++ Phaser Assignment/index.js
@@ -6,7 +6,7 @@ const config = {
     physics: {
       default: "arcade",
       arcade: {
-        gravity: { y: 100 },
+        gravity: { y: 300 },
         debug: true,
       },
       pixelArt: true,
@@ -26,6 +26,7 @@ const config = {
   function preload(){
     this.load.image("block", "./Game/Legacy-Fantasy - High Forest 2.3/Assets/battleground.png" )
     this.load.image("sprite", "./Game/Legacy-Fantasy - High Forest 2.3/Character/Attack-01/Attack-01.gif")
+    this.load.image("ball", "./Game/Legacy-Fantasy - High Forest 2.3/Assets/ballgif.gif" )
     this.load.spritesheet("attack", "./Game/Legacy-Fantasy - High Forest 2.3/Character/Attack-01/Attack-01-Sheet.png", {frameWidth: 96, frameHeight: 80, frames: 8})
     this.load.spritesheet("dead", "./Game/Legacy-Fantasy - High Forest 2.3/Character/Dead/Dead-Sheet.png", {frameWidth: 80, frameHeight: 80, frames: 9})
     this.load.spritesheet("idle", "./Game/Legacy-Fantasy - High Forest 2.3/Character/Idle/Idle-Sheet.png", {frameWidth: 64, frameHeight: 80, frames: 4});
@@ -40,6 +41,12 @@ const config = {
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     
+  this.anims.create({
+    key:"enemyBall",
+    frames: this.anims.generateFrameNumbers("ball", {start: 0, end: 3}),
+    frameRate: 10,
+    repeat: -1
+  })
     this.anims.create({
       key: "playerIdle",
       frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 3 }),
@@ -59,10 +66,13 @@ const config = {
       repeat: -1,
     })
 
-    
     this.add.tileSprite(0,0,1000,1000,"block").setOrigin(0,0);
     player = this.physics.add.sprite(50,310,"sprite");
     player.anims.play("playerIdle");
+    
+    // this.add.tileSprite(0,0,1000,1000,"ball").setOrigin(500,100);
+    // slime = this.physics.add.sprite(50,310,"ball")
+    // player.anims.play("enemyBall")
     
     player.setCollideWorldBounds(true);
     player.setSize(45,50)
--- Year of the Horse/homework.js
+++ Year of the Horse/homework.js
@@ -1,56 +1,128 @@
 function showInfo(year) {
-    // You can customize the information based on the clicked year
-    var infoDisplay = document.getElementById('infoDisplay');
-  
-    switch (year) {
+  var infoDisplay = document.getElementById('infoDisplay');
+
+  switch (year) {
+      case 2023: 
+      infoDisplay.innerHTML = 
+      "1) What are the lessons you learned?: I do not want to pursue school." + 
+      "<br>2) What are the things you want to earn?: I want to earn a potential job." +
+      "<br>3) What are the things you can afford to lose?: I can afford to lose highschool friends." +
+      "<br>4) What are the things you want to gain?: I want to gain IT experience." +
+      "<br>5) What do you want to change?: I want to be more productive and get better at Volleyball." +
+      "<br>6) What do you want to stay the same?: I want my drive and motivation to keep me going." +
+      "<br>7) What is your wish for this year?: For this year, I want to have a sense of direction." +
+      "<br>8) What is your favorite dish?: Pho." +
+      "<br>9) Make yourself a fortune based on this year. What would you call the year?: The Year of Finding Myself"
+    break;
+      case 2024:
+          infoDisplay.innerHTML = 
+          "1) What are the lessons you want to learn?: It's okay to work hard." + 
+          "<br>2) What are the things you want to earn?: I want to earn a promotion." +
+          "<br>3) What are the things you can afford to lose?: I can afford to lose some weight." +
+          "<br>4) What are the things you want to gain?: I want to gain more volleyball skills and experience." +
+          "<br>5) What do you want to change?: I want to be able to be more confident in my skills." +
+          "<br>6) What do you want to stay the same?: I want my dedication to improvement to keep driving me." +
+          "<br>7) What is your wish for this year?: Become a better volleyball player." +
+          "<br>8) What is your favorite dish?: Eggrolls" +
+          "<br>9) Make yourself a fortune based on this year. What would you call the year?: The Year of Improvement"
+        break;
       case 2025:
-        infoDisplay.innerHTML =
-          `
-          1) What are the lessons you want to learn?
-          2) What are the things you want to earn?
-          3) What are the things you can afford to lose?
-          4) What are the things you want to gain?
-          5) What do you want to change?
-          6) What do you want to stay the same?
-          7) What is your wish for this year?
-          8) What will be your favorite dish?
-          9) Make yourself a fortunate based on this year. What would you call the year?  
-          `;
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: How to move up the corporate ladder." +
+              "<br>2) What are the things you want to earn?: I want to earn more money." +
+              "<br>3) What are the things you can afford to lose?: I can afford to lose some friends." +
+              "<br>4) What are the things you want to gain?: I want to have a car." +
+              "<br>5) What do you want to change?: The loyalty I have to a company." +
+              "<br>6) What do you want to stay the same?: The motivation to keep going." +
+              "<br>7) What is your wish for this year?: Find a new opportunity to advance my career." +
+              "<br>8) What will be your favorite dish?: Fried rice." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Career Progression.";
+          break;
       case 2026:
-        infoDisplay.innerHTML =
-          `
-          1) What are the lessons you want to learn?
-          2) What are the things you want to earn?
-          3) What are the things you can afford to lose?
-          4) What are the things you want to gain?
-          5) What do you want to change?
-          6) What do you want to stay the same?
-          7) What is your wish for this year?
-          8) What will be your favorite dish?
-          9) Make yourself a fortunate based on this year. What would you call the year?  
-          `;
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Job Hopping." +
+              "<br>2) What are the things you want to earn?: I want to earn more money." +
+              "<br>3) What are the things you can afford to lose?: I can afford to spend less time with friends and family." +
+              "<br>4) What are the things you want to gain?: I want to have a house." +
+              "<br>5) What do you want to change?: I want to be able to inspire others." +
+              "<br>6) What do you want to stay the same?: The motivation to keep going." +
+              "<br>7) What is your wish for this year?: To meet Hanni Pham from NewJeans." +
+              "<br>8) What will be your favorite dish?: Turkey legs." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Continued Progress";
+          break;
       case 2027:
-        infoDisplay.innerHTML = "Information for 2027";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Raising kids are hard." +
+              "<br>2) What are the things you want to earn?: Father of the year award." +
+              "<br>3) What are the things you can afford to lose?: The amount of money I will be making." +
+              "<br>4) What are the things you want to gain?: Vacation and more time with family." +
+              "<br>5) What do you want to change?: My motivation to work so hard." +
+              "<br>6) What do you want to stay the same?: My love for work, but with restrictions." +
+              "<br>7) What is your wish for this year?: To be able to raise my kids in a nice home with enough time to spend with them." +
+              "<br>8) What will be your favorite dish?: Cereal." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Raising Newborns.";
+          break;
       case 2032:
-        infoDisplay.innerHTML = "Information for 2032";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Balance career and family responsibilities effectively." +
+              "<br>2) What are the things you want to earn?: Recognition for contributions in IT while maintaining a strong family bond." +
+              "<br>3) What are the things you can afford to lose?: The constant pursuit of promotions at the expense of quality family time." +
+              "<br>4) What are the things you want to gain?: A sense of fulfillment from both professional achievements and family milestones." +
+              "<br>5) What do you want to change?: Prioritizing family without compromising professional growth." +
+              "<br>6) What do you want to stay the same?: Dedication to work, now channeled into meaningful projects that allow for a healthier work-life balance." +
+              "<br>7) What is your wish for this year?: Creating a stable and supportive environment for the family, ensuring both career and family aspirations are met." +
+              "<br>8) What will be your favorite dish?: Korean Food." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Work Life Balance";
+          break;
       case 2037:
-        infoDisplay.innerHTML = "Information for 2037";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Utilize technology to enhance family connections." +
+              "<br>2) What are the things you want to earn?: Respect in the IT community for advocating family-friendly workplace initiatives." +
+              "<br>3) What are the things you can afford to lose?: Overcommitting to projects at the cost of personal well-being." +
+              "<br>4) What are the things you want to gain?: Innovations in IT solutions that make remote work more accessible and family-friendly." +
+              "<br>5) What do you want to change?: Implementing efficient work processes to reduce time spent away from family." +
+              "<br>6) What do you want to stay the same?: Maintaining a strong work ethic, adapting it to accommodate family needs." +
+              "<br>7) What is your wish for this year?: Create an innovation so innovative that it surpasses Google and Microsoft." +
+              "<br>8) What will be your favorite dish?: Home cooked meals." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Finding Solutions";
+          break;
       case 2042:
-        infoDisplay.innerHTML = "Information for 2042";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Transition into leadership roles." +
+              "<br>2) What are the things you want to earn?: Respect and acknowledgment." +
+              "<br>3) What are the things you can afford to lose?: Micromanagement." +
+              "<br>4) What are the things you want to gain?: Professional growth with a focus on mentorship." +
+              "<br>5) What do you want to change?: Find efficiency in work processes." +
+              "<br>6) What do you want to stay the same?: Continue to have a passion for Cybersecurity." +
+              "<br>7) What is your wish for this year?: Successfully lead a team." +
+              "<br>8) What will be your favorite dish?: Donuts." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Leadership";
+          break;
       case 2047:
-        infoDisplay.innerHTML = "Information for 2047";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: Being in a management role is boring." +
+              "<br>2) What are the things you want to earn?: I want more vacation days." +
+              "<br>3) What are the things you can afford to lose?: I can afford to lose more work time." +
+              "<br>4) What are the things you want to gain?: I want to become a CISO." +
+              "<br>5) What do you want to change?: Degrees are useless." +
+              "<br>6) What do you want to stay the same?: My laidback attitude while being a CISO." +
+              "<br>7) What is your wish for this year?: To inspire other people that they can be as successful as me without a degree." +
+              "<br>8) What will be your favorite dish?: Lobster." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Success";
+          break;
       case 2052:
-        infoDisplay.innerHTML = "Information for 2052";
-        break;
+          infoDisplay.innerHTML =
+              "1) What are the lessons you want to learn?: I do not need to work hard anymore." +
+              "<br>2) What are the things you want to earn?: An award for being the best CISO." +
+              "<br>3) What are the things you can afford to lose?: I can lose my job." +
+              "<br>4) What are the things you want to gain?: I want to gain my retirement money." +
+              "<br>5) What do you want to change?: I want to finally be able to give myself a chance to rest from the industry." +
+              "<br>6) What do you want to stay the same?: I want my love for Cybersecurity to still be there." +
+              "<br>7) What is your wish for this year?: To spend time with family." +
+              "<br>8) What will be your favorite dish?: Pho." +
+              "<br>9) Make yourself fortunate based on this year. What would you call the year?: The Year of Retirement";
+          break;
       default:
-        infoDisplay.innerHTML = "No information available";
-    }
+          infoDisplay.innerHTML = "No information available";
   }
-  
+}
